05475ffe371955fc1bdfd91902a7f8bdce7cf59a
Nbiba Bedis <bedisnbiba@gmail.com>
2025-02-21 00:04:23 +0100

    docs

--- mod_inline.ts
+++ mod_inline.ts
@@ -7,7 +7,7 @@ Infer file type. This is a wasm bindings to https://github.com/bojand/infer. It 
 
 @example
 ```ts
-import * as infer from "@sigmasd/deno-infer";
+import * as infer from "@sigmasd/infer";
 
 const type = infer.get(
   new TextEncoder().encode(
@@ -18,6 +18,14 @@ const type = infer.get(
 console.log(type.extension());
 console.log(type.matcherType());
 console.log(type.mimeType());
+```
+
+## Inline Module
+
+Runtimes that dont support wasm import can use the `inline` module which have the wasm file inlined.
+
+```ts
+import * as infer from "jsr:@sigmasd/deno-infer/inline";
 ```
 @module
 */
